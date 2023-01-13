60b9fec78c37952d4d9cfe655cef1e9255ad215f
Shahidafridi3303 <115124671+Shahidafridi3303@users.noreply.github.com>
2023-01-13 00:52:15 -0500

    added slider control to move player

--- GAME3001-W2023-Lab1/src/PlayScene.cpp
+++ GAME3001-W2023-Lab1/src/PlayScene.cpp
@@ -36,48 +36,19 @@ void PlayScene::HandleEvents()
 {
 	EventManager::Instance().Update();
 
-	// handle player movement with GameController
-	if (SDL_NumJoysticks() > 0)
-	{
-		if (EventManager::Instance().GetGameController(0) != nullptr)
-		{
-			const auto deadZone = 10000;
-			if (EventManager::Instance().GetGameController(0)->STICK_LEFT_HORIZONTAL > deadZone)
-			{
-				m_pPlayer->SetAnimationState(PlayerAnimationState::PLAYER_RUN_RIGHT);
-				m_playerFacingRight = true;
-			}
-			else if (EventManager::Instance().GetGameController(0)->STICK_LEFT_HORIZONTAL < -deadZone)
-			{
-				m_pPlayer->SetAnimationState(PlayerAnimationState::PLAYER_RUN_LEFT);
-				m_playerFacingRight = false;
-			}
-			else
-			{
-				if (m_playerFacingRight)
-				{
-					m_pPlayer->SetAnimationState(PlayerAnimationState::PLAYER_IDLE_RIGHT);
-				}
-				else
-				{
-					m_pPlayer->SetAnimationState(PlayerAnimationState::PLAYER_IDLE_LEFT);
-				}
-			}
-		}
-	}
-
-
 	// handle player movement if no Game Controllers found
 	if (SDL_NumJoysticks() < 1)
 	{
 		if (EventManager::Instance().IsKeyDown(SDL_SCANCODE_A))
 		{
 			m_pPlayer->SetAnimationState(PlayerAnimationState::PLAYER_RUN_LEFT);
+			m_pPlayer->GetTransform()->position -= glm::vec2(5.0f, 0.0f);
 			m_playerFacingRight = false;
 		}
 		else if (EventManager::Instance().IsKeyDown(SDL_SCANCODE_D))
 		{
 			m_pPlayer->SetAnimationState(PlayerAnimationState::PLAYER_RUN_RIGHT);
+			m_pPlayer->GetTransform()->position += glm::vec2(5.0f, 0.0f);
 			m_playerFacingRight = true;
 		}
 		else
@@ -132,22 +103,23 @@ void PlayScene::GUI_Function() const
 	// See examples by uncommenting the following - also look at imgui_demo.cpp in the IMGUI filter
 	//ImGui::ShowDemoWindow();
 	
-	ImGui::Begin("Your Window Title Goes Here", NULL, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoMove);
+	ImGui::Begin("GAME3001 -W2023 - Lab1", NULL, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_MenuBar );
 
-	if(ImGui::Button("My Button"))
+	float float2[2] = { m_pPlayer->GetTransform()->position.x, m_pPlayer->GetTransform()->position.y };
+	if(ImGui::SliderFloat2("Player Position", float2, 0.0f, 800.0f))
 	{
-		std::cout << "My Button Pressed" << std::endl;
-	}
+		if(float2[0] > m_pPlayer->GetTransform()->position.x)
+		{
+			//moving right
+			m_pPlayer->SetAnimationState(PlayerAnimationState::PLAYER_RUN_RIGHT);
+		}
 
-	ImGui::Separator();
+		if (float2[0] > m_pPlayer->GetTransform()->position.x)
+		{
+			m_pPlayer->SetAnimationState(PlayerAnimationState::PLAYER_RUN_LEFT);
+		}
 
-	static float float3[3] = { 0.0f, 1.0f, 1.5f };
-	if(ImGui::SliderFloat3("My Slider", float3, 0.0f, 2.0f))
-	{
-		std::cout << float3[0] << std::endl;
-		std::cout << float3[1] << std::endl;
-		std::cout << float3[2] << std::endl;
-		std::cout << "---------------------------\n";
+		m_pPlayer->GetTransform()->position = glm::vec2(float2[0], float2[1]);
 	}
 	
 	ImGui::End();
