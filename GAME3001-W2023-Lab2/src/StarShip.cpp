0774ec34a24dedcc1ebb75a898c9864e56b6b8bb
Shahidafridi3303 <115124671+Shahidafridi3303@users.noreply.github.com>
2023-01-24 12:23:07 -0500

    added StarShip class to project

--- GAME3001-W2023-Lab2/src/StarShip.cpp
+++ GAME3001-W2023-Lab2/src/StarShip.cpp
@@ -1,1 +1,34 @@
 #include "StarShip.h"
+#include "TextureManager.h"
+
+StarShip::StarShip()
+{
+	TextureManager::Instance().Load("../Assets/textures/ncl.png", "starship");
+
+	const auto size = TextureManager::Instance().GetTextureSize("starship");
+	SetWidth(static_cast<int>(size.x));
+	SetHeight(static_cast<int>(size.y));
+	GetTransform()->position = glm::vec2(100.0f, 100.0f);
+	GetRigidBody()->velocity = glm::vec2(0, 0);
+	GetRigidBody()->isColliding = false;
+
+	SetType(GameObjectType::STARSHIP);
+}
+
+StarShip::~StarShip()
+= default;
+
+void StarShip::Draw()
+{
+	// draw the target
+	TextureManager::Instance().Draw("starship",
+		GetTransform()->position, 0, 255, true);
+}
+
+void StarShip::Update()
+{
+}
+
+void StarShip::Clean()
+{
+}
--- GAME3001-W2023-Lab2/src/StarShip.h
+++ GAME3001-W2023-Lab2/src/StarShip.h
@@ -1,10 +1,18 @@
 #pragma once
 #ifndef __STARSHIP__
 #define __STARSHIP__
+
 #include "DisplayObject.h"
 class StarShip : public DisplayObject
 {
 public:
+	// Constructor Function
+	StarShip();
+
+	//Destructor Function
+	~StarShip();
+
+	// LifeCycle Functions
 	void Draw() override;
 	void Update() override;
 	void Clean() override;
@@ -14,4 +22,3 @@ private:
 };
 
 #endif /* defined ( STARSHIP )*/
-
