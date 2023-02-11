5d4b90e79982299e2e3ce57db256260d9de6c62e
Shahidafridi3303 <115124671+Shahidafridi3303@users.noreply.github.com>
2023-02-11 15:05:45 -0500

    Added project files for Lab4

---
+++ GAME3001-W2023-Lab4/src/PlayScene.cpp
@@ -0,0 +1,113 @@
+#include "PlayScene.h"
+#include "Game.h"
+#include "EventManager.h"
+
+// required for IMGUI
+#include "imgui.h"
+#include "imgui_sdl.h"
+#include "Renderer.h"
+#include "Util.h"
+
+PlayScene::PlayScene()
+{
+	PlayScene::Start();
+}
+
+PlayScene::~PlayScene()
+= default;
+
+void PlayScene::Draw()
+{
+	DrawDisplayList();
+
+	SDL_SetRenderDrawColor(Renderer::Instance().GetRenderer(), 255, 255, 255, 255);
+}
+
+void PlayScene::Update()
+{
+	UpdateDisplayList();
+}
+
+void PlayScene::Clean()
+{
+	RemoveAllChildren();
+}
+
+void PlayScene::HandleEvents()
+{
+	EventManager::Instance().Update();
+
+	if (EventManager::Instance().IsKeyDown(SDL_SCANCODE_ESCAPE))
+	{
+		Game::Instance().Quit();
+	}
+
+	if (EventManager::Instance().IsKeyDown(SDL_SCANCODE_1))
+	{
+		Game::Instance().ChangeSceneState(SceneState::START);
+	}
+
+	if (EventManager::Instance().IsKeyDown(SDL_SCANCODE_2))
+	{
+		Game::Instance().ChangeSceneState(SceneState::END);
+	}
+}
+
+void PlayScene::Start()
+{
+	// Set GUI Title
+	m_guiTitle = "Play Scene";
+	m_bDebugView = false; // turn off debug colliders
+
+	// Add the Target to the Scene
+	m_pTarget = new Target(); // instantiate an object of type Target
+	m_pTarget->GetTransform()->position = glm::vec2(640.0f, 300.0f);
+	AddChild(m_pTarget);
+
+	// Add the StarShip to the Scene
+	m_pStarShip = new StarShip();
+	m_pStarShip->GetTransform()->position = glm::vec2(100.0f, 300.0f);
+	AddChild(m_pStarShip);
+
+
+	// Preload Sounds
+
+	SoundManager::Instance().Load("../Assets/Audio/yay.ogg", "yay", SoundType::SOUND_SFX);
+	SoundManager::Instance().Load("../Assets/Audio/thunder.ogg", "thunder", SoundType::SOUND_SFX);
+
+	ImGuiWindowFrame::Instance().SetGuiFunction(std::bind(&PlayScene::GUI_Function, this));
+}
+
+void PlayScene::GUI_Function()
+{
+	// Always open with a NewFrame
+	ImGui::NewFrame();
+
+	// See examples by uncommenting the following - also look at imgui_demo.cpp in the IMGUI filter
+	//ImGui::ShowDemoWindow();
+	
+	ImGui::Begin("GAME3001 - W2023 - Lab4", NULL, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_MenuBar );
+
+	ImGui::Separator();
+
+	// Debug Properties
+	static bool toggleDebug = false;
+	if(ImGui::Checkbox("Toggle Debug View", &toggleDebug))
+	{
+		m_bDebugView = toggleDebug;
+	}
+
+	ImGui::Separator();
+
+	// StarShip Properties
+
+
+	ImGui::Separator();
+
+
+	// Target Properties
+
+	ImGui::Separator();
+
+	ImGui::End();
+}
