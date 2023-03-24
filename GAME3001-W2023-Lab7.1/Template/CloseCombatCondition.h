a680d228f7adb2b6cc44202b325e55cbd41ec336
Shahidafridi3303 <115124671+Shahidafridi3303@users.noreply.github.com>
2023-03-24 15:13:55 -0400

    Added condition nodes

---
+++ GAME3001-W2023-Lab7.1/Template/CloseCombatCondition.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef __CLOSE_COMBAT__CONDITION__
+#define __CLOSE_COMBAT__CONDITION__
+
+#include "Condition"
+
+class CloseCombatCondition : public 
+
+#endif  /* defined (__CLOSE_COMBAT__CONDITION__) */
+ 
